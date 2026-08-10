4515a8d8a2dc3c05361b17a2d3554d53146900e8
karamegy <79487897+karamegy@users.noreply.github.com>
2026-08-10 03:02:18 +0300

    Log Firebase initialization in asw.js
    Added console log to confirm Firebase initialization.

--- asw.js
+++ asw.js
@@ -1,6 +1,8 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAekw5iD-7t-BeRbrVBykXNWqtY0C7y7ug",
   authDomain: "tarazan-284f3.firebaseapp.com",
@@ -11,5 +13,8 @@ const firebaseConfig = {
   measurementId: "G-KCRSY63RTC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+console.log("Firebase initialized from asw.js");
